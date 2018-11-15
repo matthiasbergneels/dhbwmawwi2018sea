c34f997a4ef82efed8d0c15f639ab9052baeeb4f
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2018-11-15 18:49:14 +0100

    Chapter 5 example

---
+++ src/kapitel5/Car.java
@@ -0,0 +1,79 @@
+package kapitel5;
+
+public class Car {
+
+    private Colors color;
+    private int hp;
+    private String licensePlate;
+    private String brand;
+    private double currentSpeed;
+
+
+    enum Colors{
+        SCHWARZ, ROT, LILA, BLAU, GRUEN
+    }
+
+
+    Car(Colors color, int hp, String licensePlate, String brand){
+        this.setColor(color);
+        this.setHp(hp);
+        this.setLicensePlate(licensePlate);
+        this.setBrand(brand);
+
+        this.currentSpeed = 0.0;
+
+    }
+
+    public Colors getColor(){
+        return color;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void setColor(Colors color){
+        this.color = color;
+    }
+
+    public void setHp(int hp) {
+        if(hp > 25 && hp < 500){
+            this.hp = hp;
+        }else{
+            this.hp = 75;
+        }
+    }
+
+    private void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    void accelerate(double speed){
+        currentSpeed = currentSpeed + speed;
+    }
+
+    void brake(double speed){
+        currentSpeed = currentSpeed - speed;
+    }
+
+    double howFastIsOurCar(){
+        return currentSpeed;
+    }
+
+}
---
+++ src/kapitel5/CarTest.java
@@ -0,0 +1,47 @@
+package kapitel5;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car(Car.Colors.GRUEN, 150, "HD-XX 1234", "BMW");
+
+        /* Initialisiert durch Konstruktor
+        myCar.color = "lila";
+        myCar.licensePlate = "HD-XX 1234";
+        myCar.hp = 124;
+        myCar.brand = "BMW";
+        */
+
+        myCar.setColor(Car.Colors.LILA);
+
+        Car yourCar = new Car(Car.Colors.ROT, -99, "MA-YY 6789", "Fiat");
+
+        yourCar.setColor(Car.Colors.SCHWARZ);
+        //yourCar.setHp(-99);
+
+        /* Initialisiert durch Konstruktor
+        yourCar.color = "blau";
+        yourCar.licensePlate = "MA-YY 6789";
+        yourCar.hp = 90;
+        yourCar.brand = "Fiat";
+        */
+
+        System.out.println("Farbe von myCar: " + myCar.getColor());
+        System.out.println("Nummernschild von myCar: " + myCar.getLicensePlate());
+
+        System.out.println("Farbe von yourCar: " + yourCar.getColor());
+        System.out.println("Nummernschild von yourCar: " + yourCar.getLicensePlate());
+
+        myCar.accelerate(50);
+        myCar.accelerate(30);
+
+        System.out.println("Aktuelle Geschwindigkeit von myCar: " + myCar.getCurrentSpeed());
+
+        myCar.brake(35);
+
+        System.out.println("Aktuelle Geschwindigkeit von myCar: " + myCar.getCurrentSpeed());
+
+        System.out.println("Aktuelle Geschwindigkeit von yourCar: " + yourCar.getCurrentSpeed());
+    }
+}
