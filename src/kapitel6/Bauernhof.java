6959dd14d6e9b862e83bfdad67b92ca0df083285
Matthias <matthiasuweberg@icloud.com>
2018-11-29 18:56:35 +0100

    Polymorphie und Casting

--- src/kapitel6/Bauernhof.java
+++ src/kapitel6/Bauernhof.java
@@ -21,8 +21,52 @@ public class Bauernhof {
         nemo.bewegen();
         nemo.fressen();
         nemo.atmet();
+        
+        
+        // Narrowing Cast
+        Tier einTier = bello;
+        
+        einTier.bewegen();
+        einTier.fressen();
+        einTier.atmen();
+        //einTier.bellen();
+        
+        // Narrowing Cast
+        einTier = nemo;
+        
+        einTier.bewegen();
+        einTier.fressen();
+        einTier.atmen();
+        //einTier.blubbern();
+        
+        // Narrowing Cast
+        Object tiereSindObjekte = bello;
+        
+        System.out.println(tiereSindObjekte.toString());
 
-
+        
+        Tier[] tierboxen = new Tier[4];
+        
+        tierboxen[0] = bello;
+        tierboxen[1] = nemo;
+        tierboxen[2] = new Hund(15.0f, 30.0f, "Benno", "Schäferhund");
+        tierboxen[3] = new Fisch(5.0f, 3.5f, "Marlene", false);
+        
+        for(Tier aktuellesTier : tierboxen){
+            aktuellesTier.bewegen();
+            aktuellesTier.atmen();
+            aktuellesTier.fressen();
+            
+            if(aktuellesTier instanceof Hund){
+                Hund einHund = (Hund)aktuellesTier;
+                einHund.bellen();
+            }
+            
+        }
+        
+        // Widening Cast
+        Hund einHund = (Hund)tierseSindObjekte;
+        
         System.out.println(bello);
         System.out.println(nemo);
     }
