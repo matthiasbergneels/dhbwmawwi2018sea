19d1f3d876163163852938918fae446301afd8b1
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2019-03-07 18:38:52 +0100

    Skript und Übungen

--- src/uebungen/Schaltjahr.java
+++ src/uebungen/Schaltjahr.java
@@ -1,55 +1,44 @@
 package uebungen;
 
+import kapitel8.YearNotInRange;
+
 public class Schaltjahr {
 
     public static void main(String[] args) {
 
-        int jahr = 2100; //Integer.parseInt(args[0]);
-        int istSchaltjahr;
+        int jahr = 2000; //Integer.parseInt(args[0]);
 
-        istSchaltjahr = pruefeSchaltjahr(jahr);
-
-        if (istSchaltjahr == 1) {
-            System.out.println("Das Jahr " + jahr +
-                    " ist ein Schaltjahr.");
-        } else if (istSchaltjahr == 2) {
-            System.out.println("Das Jahr " + jahr +
-                    " ist kein Schaltjahr.");
-        } else {
-            System.out.println("Bitte geben Sie ein " +
-                    "gültiges Jahr ein!");
+        try {
+            if (pruefeSchaltjahr(jahr)) {
+                System.out.println("Das Jahr " + jahr +
+                        " ist ein Schaltjahr.");
+            } else {
+                System.out.println("Das Jahr " + jahr +
+                        " ist kein Schaltjahr.");
+            }
+            return;
+        }catch (YearNotInRange e){
+            System.out.println("Bitte geben Sie ein gültiges Jahr ein!");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }catch (Exception e){
+            System.out.println("Bitte geben Sie ein gültiges Jahr ein!");
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }finally {
+            System.out.println("WIRKLICH Nichts führt an mir vorbei!");
         }
     }
 
-    private static int pruefeSchaltjahr(int jahr) {
+    private static boolean pruefeSchaltjahr(int jahr) throws YearNotInRange {
 
         if(jahr < 1500 || jahr > 3000){
-            return 0;
+            throw new YearNotInRange(jahr);
         }
+
 
         // Prüfung auf Schaltjahr
-        if(jahr % 4 == 0 && jahr % 100 != 0 || jahr % 400 == 0){
-            return 1;
-        }else{
-            return 2;
-        }
-
-        /*
-        if(jahr % 4 == 0){
-            if(jahr % 100 == 0){
-                if(jahr % 400 == 0){
-                    return 1;
-                }else{
-                    return 2;
-                }
-            }else{
-                return 1;
-            }
-        }else{
-            return 2;
-        }
-
-        */
+        return (jahr % 4 == 0 && jahr % 100 != 0 || jahr % 400 == 0);
 
     }
 
